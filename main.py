fffe6e19f07382bfb5788f7d05761fc914f15e41
montepresident <neilchristianmontemayor@gmail.com>
2024-12-05 17:09:23 -0800

    Added FPS locked to 60 FPS

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     on = True
     black = 000000
+    clock = pygame.time.Clock()
+    dt = 0
+
     while on == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
 
