ecc39ce450e040914a8980fd410afa043e030ab8
montepresident <neilchristianmontemayor@gmail.com>
2024-12-06 14:36:56 -0800

    Mistakenly added as part of main.py; creating new file

--- main.py
+++ main.py
@@ -19,26 +19,7 @@ def main():
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
 
-
-
-# Base class for game objects #####
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
-
-    def draw(self, screen):
-        pass
-
-    def update(self, dt):
-        pass
+    
 
 if __name__ == "__main__":
 	main()
